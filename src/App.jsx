b9d811cfeb846c1975169de42ed0bf354081322e
Sylvain_C <scarlier@cutii.io>
2021-03-04 16:04:54 +0100

    upload project

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,11 @@ function App() {
 
 
 
+<<<<<<< HEAD
   const moviesurl = "https://www.omdbapi.com/?apikey=bdcbe34b";
+=======
+  const moviesurl = "http://www.omdbapi.com/?apikey=bdcbe34b";
+>>>>>>> a941fc4... upload project
 
   const search = (e) => {
     if (e.key === "Enter") {
